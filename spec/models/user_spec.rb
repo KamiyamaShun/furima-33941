9c2efdb76afd48bc7c6fd51b0af9e69c7fcad80c
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-09 14:48:35 +0900

    test model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,41 +6,53 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-    it 'すべて入力したら登録ができる' do
-    end
-
     it 'nicknameが空では登録できない' do
+      @user.nickname = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
 
     it 'emailが空では登録できない' do
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
 
     it 'すでに登録されたemailでは登録できない' do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
 
     it 'passwordがなければ登録できない' do
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
     end
 
     it 'passwordが5文字では登録ができない' do
+      @user.password = 'abc12'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
 
     it 'passwaordが数字のみでは登録ができない' do
+      @user.password = '123456'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password 英数字混合で入力してください')
     end
 
     it 'password_confirmationがなければ登録ができない' do
+      @user.password_confirmation = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
 
-    it 'passwaordが数字のみでは登録ができない' do
+    it '誕生日が入力されていなければ登録できない' do
+      @user.birth = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birth can't be blank")
     end
-  end
-
-  describe 'ユーザーログイン'
-  it 'すべて入力したらログインができる' do
-  end
-
-  it 'emailが空では登録ができない' do
-  end
-
-  it 'passwordが空では登録できない' do
   end
 end
