f076417f03cd50944a340b14754506ad072d524f
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-09 12:21:32 +0900

    Rubocop do

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,42 +6,41 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-    it "すべて入力したら登録ができる" do
+    it 'すべて入力したら登録ができる' do
     end
 
-    it "nicknameが空では登録できない" do
+    it 'nicknameが空では登録できない' do
     end
 
-    it "emailが空では登録できない" do
+    it 'emailが空では登録できない' do
     end
 
-    it "すでに登録されたemailでは登録できない" do
+    it 'すでに登録されたemailでは登録できない' do
     end
 
-    it "passwordがなければ登録できない" do
+    it 'passwordがなければ登録できない' do
     end
 
-    it "passwordが5文字では登録ができない" do
+    it 'passwordが5文字では登録ができない' do
     end
 
-    it "passwaordが数字のみでは登録ができない" do
+    it 'passwaordが数字のみでは登録ができない' do
     end
 
-    it "password_confirmationがなければ登録ができない" do
+    it 'password_confirmationがなければ登録ができない' do
     end
 
-    it "passwaordが数字のみでは登録ができない" do
+    it 'passwaordが数字のみでは登録ができない' do
     end
-
   end
 
   describe 'ユーザーログイン'
-    it "すべて入力したらログインができる" do
-    end
+  it 'すべて入力したらログインができる' do
+  end
 
-    it "emailが空では登録ができない" do
-    end
+  it 'emailが空では登録ができない' do
+  end
 
-    it "passwordが空では登録できない" do
-    end
+  it 'passwordが空では登録できない' do
+  end
 end
