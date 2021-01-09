2d35e36439bc0a6c0a62bba32669a0bf71d86857
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-09 11:58:41 +0900

    model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,43 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
-  
+  describe 'ユーザー新規登録' do
+    it "すべて入力したら登録ができる" do
+    end
+
+    it "nicknameが空では登録できない" do
+    end
+
+    it "emailが空では登録できない" do
+    end
+
+    it "すでに登録されたemailでは登録できない" do
+    end
+
+    it "passwordがなければ登録できない" do
+    end
+
+    it "passwordが5文字では登録ができない" do
+    end
+
+    it "passwaordが数字のみでは登録ができない" do
+    end
+
+    it "password_confirmationがなければ登録ができない" do
+    end
+
+    it "passwaordが数字のみでは登録ができない" do
+    end
+
+  end
+
+  describe 'ユーザーログイン'
+    it "すべて入力したらログインができる" do
+    end
+
+    it "emailが空では登録ができない" do
+    end
+
+    it "passwordが空では登録できない" do
+    end
 end
