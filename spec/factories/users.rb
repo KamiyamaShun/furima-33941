932ae439f6b62aa5372eb67d9888707cd99cb22c
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-09 11:02:02 +0900

    rspec/factorybot install

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"test"}
+    family_name           {"ああ"}
+    first_name            {"あ"}
+    family_name_kana      {"アア"}
+    first_name_kana       {"ア"}
+    birth                 {1930-1-1}
+    email                 {"test@example"}
+    password              {"abc123"}
+    password_confirmation {password}
+  end
+end
