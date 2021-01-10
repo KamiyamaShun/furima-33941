3374cc83e1c9958e5057f0952e00fbd8a6b2f807
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-10 11:57:35 +0900

    comment out params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,12 +7,12 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new(item_params)
+    @item = Item.new
     @item.save
   end
 
   private
-  def item_params
-    params.require(:items).permit(:name, :info, :category_id, :status_id, :ship_fee_id, :prefecture_id, :ship_date_id, :price, :image),merge(user_id: current_user.id)
-  end
+  # def item_params
+  #   params.require(:items).permit(:name, :info, :category_id, :status_id, :ship_fee_id, :prefecture_id, :ship_date_id, :price, :image),merge(user_id: current_user.id)
+  # end
 end
