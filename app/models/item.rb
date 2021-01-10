4160091b8541e88ad4b7bb0178b9387b709b93e5
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-10 11:57:21 +0900

    mk active_hash assosiation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,26 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   with_options presence: true do
     validates :name
     validates :info
+    validates :price
+  end
+  
+  with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :status_id
     validates :ship_fee_id
     validates :prefecture_id
     validates :ship_date_id
-    validates :price
   end
+  
   has_one_attached :image
 
   belongs_to :user
+
+  belongs_to :category
+  belongs_to :status
+  belongs_to :ship_fee
+  belongs_to :prefecture
+  belongs_to :ship_date
 end
