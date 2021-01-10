85c871faa97dfa78384333b5f5068b3028eb4b73
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-11 08:55:58 +0900

    prce javascript add

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,9 +3,10 @@ function price(){
   priceInput.addEventListener("input",() => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = inputValue * 0.1 ;
+    const priceCalc = Math.floor(inputValue * 0.1);
+    addTaxDom.innerHTML = priceCalc ;
     const addGainDom = document.getElementById("profit");
-    addGainDom.innerHTML = inputValue - (inputValue * 0.1);
+    addGainDom.innerHTML = inputValue - priceCalc;
   });
 }
 window.addEventListener('load',price);
