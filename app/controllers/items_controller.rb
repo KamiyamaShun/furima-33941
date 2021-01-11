c3e64d0ab6b957d8f6baaa5349cfbb3987defeb2
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-11 19:12:24 +0900

    move_to_index add

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
   before_action :set_instance, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
