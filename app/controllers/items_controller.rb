84a86adbc89c0bf67aed89eac88b927770be2832
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-11 15:59:44 +0900

    add action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_instance, only: [:show, :edit]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -18,10 +19,20 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :info, :category_id, :status_id, :ship_fee_id, :prefecture_id, :ship_date_id, :price,
                                  :image).merge(user_id: current_user.id)
+  end
+
+  def set_instance
+    @item = Item.find(params[:id])
   end
 end
