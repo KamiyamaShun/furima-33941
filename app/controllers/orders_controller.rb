9a7e690fd26c7031df367d94f188334c589871d8
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-12 12:22:13 +0900

    orders_controller set

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,20 @@
 class OrdersController < ApplicationController
   def index
+    @history_order = HistoryOrder.new
+  end
+
+  def create
+    @history_order = HistoryOrder.new(history_params)
+    if @history_order.valid?
+      @history_order.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def 
+    history_params.require(:history_order).permit(:post_number, :prefecture_id, :city, :address, :building, :phone_number)
   end
 end
