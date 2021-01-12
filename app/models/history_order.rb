cc639179118b3ff14b45f8cc501f357bbf4eed8e
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-12 19:26:28 +0900

    validation change

--- app/models/history_order.rb
+++ app/models/history_order.rb
@@ -3,12 +3,12 @@ class HistoryOrder
   attr_accessor :post_number, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :post_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "ハイフンをつけて半角数字で入力してください。"}
+    validates :post_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "ハイフンをつけて半角数字で入力してください"}
     validates :city
     validates :address
-    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "ハイフンを抜いて11桁以内で入力してください。"}
+    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "ハイフンを抜いて11桁以内で入力してください"}
   end
-  validates :prefecture_id, numericality: { other_than: 0, message: "都道府県を選んでください。" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "都道府県を選んでください" }
 
   def save
     history = History.create(user_id: user_id, item_id: item_id)
