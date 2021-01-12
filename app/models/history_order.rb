ecd10e73ca0b2ad222799981102b8d1f112985b5
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-12 12:14:32 +0900

    form_object_patern set

--- app/models/history_order.rb
+++ app/models/history_order.rb
@@ -1,4 +1,17 @@
 class HistoryOrder
   include ActiveModel::Model
   attr_accessor :post_number, :prefecture_id, :city, :address, :building, :phone_number
+
+  with_options presence: true do
+    validates :post_number, format{ with: /\A[0-9]{3}-[0-9]{4}\z/, message: "ハイフンをつけて半角数字で入力してください。"}
+    validates :city
+    validates :address
+    validates :phone_number, format{with: /\A[0-9]{11}\z/, message: "ハイフンを抜いて11桁以内で入力してください。"}
+  end
+  validates :prefecture_id, numericality: { other_than: 0, message: "都道府県を選んでください。" }
+
+  def save
+    history = HIstory.create(user_id: current_user.id, item_id: item_id)
+    Order.create(post_number: post_number, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, history_id: history.id)
+  end
 end
