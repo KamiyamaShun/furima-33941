83ebdd063ef121185a97ff0b077b22263a2ff99d
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-12 12:32:08 +0900

    fix model

--- app/models/history_order.rb
+++ app/models/history_order.rb
@@ -3,10 +3,10 @@ class HistoryOrder
   attr_accessor :post_number, :prefecture_id, :city, :address, :building, :phone_number
 
   with_options presence: true do
-    validates :post_number, format{ with: /\A[0-9]{3}-[0-9]{4}\z/, message: "ハイフンをつけて半角数字で入力してください。"}
+    validates :post_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "ハイフンをつけて半角数字で入力してください。"}
     validates :city
     validates :address
-    validates :phone_number, format{with: /\A[0-9]{11}\z/, message: "ハイフンを抜いて11桁以内で入力してください。"}
+    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "ハイフンを抜いて11桁以内で入力してください。"}
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "都道府県を選んでください。" }
 
