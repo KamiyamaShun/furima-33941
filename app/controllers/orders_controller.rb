766137fa9d6164e3324d43e10765fd66aaa64483
KamiyamaShun <76637787+KamiyamaShun@users.noreply.github.com>
2021-01-12 17:48:54 +0900

    fix controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,16 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create, :move_to_root]
+  before_action :move_to_root, only: [:index, :create]
+
   def index
     @history_order = HistoryOrder.new
-    @item = Item.find(params[:item_id])
+    # @item = Item.find(params[:item_id])
   end
 
   def create
     @history_order = HistoryOrder.new(history_params)
-    @item = Item.find(params[:item_id])
+    # @item = Item.find(params[:item_id])
     if @history_order.valid?
       @history_order.save
       redirect_to root_path
@@ -18,5 +22,16 @@ class OrdersController < ApplicationController
   private
   def history_params
     params.require(:history_order).permit(:post_number, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_root
+    
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
